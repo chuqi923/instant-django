d20bca85ced8862bd8cb02ff797c48bae29b5202
chuqi <chu@deltec.ne.jp>
2021-06-17 14:56:52 +0900

    setting修正,試しに改造2

--- config/settings.py
+++ config/settings.py
@@ -204,13 +204,8 @@ if 'DYNO' in os.environ:
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
